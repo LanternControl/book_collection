6f6a67f30781a372188967215fd1f3a55d806daa
LanternControl <brett4president@hotmail.com>
2017-09-21 11:33:13 -0400

    Got signup page up, still no function

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
+require 'bcrypt'
 class User < ApplicationRecord
-  has_secure_password
+  include BCrypt
+  def password
+  @password ||= Password.new(password_hash)
+ end
+
+ def password=(new_password)
+  @password = Password.create(new_password)
+  self.password_hash = @password
+ end
+
 end
