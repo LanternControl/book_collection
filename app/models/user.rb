c072712cccdd0102c01b9ffdac5756a7bf685e10
LanternControl <brett4president@hotmail.com>
2017-09-21 13:08:23 -0400

    Got login/logout functionality. Working on sessions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,15 @@
-require 'bcrypt'
+# require 'bcrypt'
 class User < ApplicationRecord
-  include BCrypt
-  def password
-  @password ||= Password.new(password_hash)
- end
 
- def password=(new_password)
-  @password = Password.create(new_password)
-  self.password_hash = @password
- end
+  has_secure_password
+ #  include BCrypt
+ #  def password
+ #  @password ||= Password.new(password_hash)
+ # end
+ #
+ # def password=(new_password)
+ #  @password = Password.create(new_password)
+ #  self.password_hash = @password
+ # end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,12 @@ Rails.application.routes.draw do
   resources :books
 
   root 'welcome#index'
-  
+
   get 'signup'  => 'users#new'
   resources :users
+
+  get '/login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
